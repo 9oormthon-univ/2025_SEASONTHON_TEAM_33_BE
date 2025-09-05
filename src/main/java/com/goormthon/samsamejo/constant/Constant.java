93043bae9b08ffcc165bda1ca7f0765072fbf06a
prefer52 <prefer0502@gmail.com>
2025-09-06 00:30:31 +0900

    feat: 상수 및 에러 정의 #3

--- src/main/java/com/goormthon/samsamejo/constant/Constant.java
+++ src/main/java/com/goormthon/samsamejo/constant/Constant.java
@@ -4,8 +4,32 @@ import java.util.List;
 
 public class Constant {
 
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    public static final String BEARER_PREFIX = "Bearer ";
+    public static final String USER_ID_CLAIM_NAME = "uid";
+    public static final String USER_ROLE_CLAIM_NAME = "rol";
+
+    public static final String ACCESS_TOKEN = "access_token";
+    public static final String REFRESH_TOKEN = "refresh_token";
+
+    public static final String TOKEN_COOKIE_PATH = "/";
+
+    public static final String RESPONSE_CONTENT_TYPE = "application/json";
+    public static final String RESPONSE_CHARACTER_ENCODING = "UTF-8";
+
     public static final List<String> PERMIT_ALL_URLS = List.of(
-            "/api/v1/oauth2/**",
+            "/favicon.ico",
+
+            "/login/oauth2/code/google",
+            "/oauth2/authorization/google",
+            "/login/oauth2/code/kakao",
+            "/oauth2/authorization/kakao",
+            "/login/oauth2/code/naver",
+            "/oauth2/authorization/naver",
+            "/favicon.ico",
+            "/login/**",
+
+            "/api/v1/recruitments/**",
 
             "/swagger",
             "/swagger-ui.html",
@@ -15,5 +39,24 @@ public class Constant {
             "/api-docs/**",
             "/swagger-ui",
             "/v3/**"
+    );
+
+    public static final List<String> USER_AUTH_URLS = List.of(
+            "/api/v1/auth/**",
+
+            "/api/v1/recruitments/*/write",
+            "/api/v1/recruitments/crawl",
+            "/api/v1/recruitments/status",
+
+            "/api/v1/user-recruitments/**",
+
+            "/api/v1/archives/**"
+    );
+
+    public static final List<String> ADMIN_ONLY_URLS = List.of(
+            "/api/v1/recruitments/crawl",
+            "/api/v1/recruitments/status",
+
+            "/api/v1/ai/**"
     );
 }
--- src/main/java/com/goormthon/samsamejo/exception/ErrorCode.java
+++ src/main/java/com/goormthon/samsamejo/exception/ErrorCode.java
@@ -11,15 +11,19 @@ public enum ErrorCode {
     // Not Found Error
     NOT_FOUND_USER(HttpStatus.NOT_FOUND, 4000, "존재하지 않는 사용자입니다"),
     NOT_FOUND_FILE_PATH(HttpStatus.NOT_FOUND, 4001, "존재하지 않는 파일 경로입니다"),
+    NOT_FOUND_END_POINT(HttpStatus.NOT_FOUND, 4002, "존재하지 않는 요청 경로입니다."),
 
     // Security Error
-    ACCESS_DENIED_ERROR(HttpStatus.UNAUTHORIZED, 4030, "접근이 거부된 토큰입니다."),
-    TOKEN_INVALID_ERROR(HttpStatus.UNAUTHORIZED, 4031, "유효하지 않은 토큰입니다."),
-    TOKEN_EXPIRED_ERROR(HttpStatus.UNAUTHORIZED, 4032, "만료된 토큰입니다."),
+    ACCESS_DENIED(HttpStatus.UNAUTHORIZED, 4030, "접근이 거부된 토큰입니다."),
+    TOKEN_INVALID(HttpStatus.UNAUTHORIZED, 4031, "유효하지 않은 토큰입니다."),
+    TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, 4032, "만료된 토큰입니다."),
     TOKEN_TYPE_ERROR(HttpStatus.UNAUTHORIZED, 4033, "토큰 타입 오류입니다."),
-    TOKEN_UNSUPPORTED_ERROR(HttpStatus.UNAUTHORIZED, 4034, "지원하지 않는 토큰입니다."),
-    TOKEN_UNKNOWN_ERROR(HttpStatus.UNAUTHORIZED, 4035, "알 수 없는 토큰 오류입니다."),
-    TOKEN_MALFORMED_ERROR(HttpStatus.UNAUTHORIZED, 4036, "토큰 형식이 올바르지 않습니다."),
+    TOKEN_UNSUPPORTED(HttpStatus.UNAUTHORIZED, 4034, "지원하지 않는 토큰입니다."),
+    UNKNOWN_TOKEN(HttpStatus.UNAUTHORIZED, 4035, "알 수 없는 토큰 오류입니다."),
+    MALFORMED_TOKEN(HttpStatus.UNAUTHORIZED, 4036, "토큰 형식이 올바르지 않습니다."),
+    LOGIN_FAILURE(HttpStatus.UNAUTHORIZED, 4037, "로그인에 실패했습니다."),
+    NO_REFRESH_TOKEN_HEADER(HttpStatus.UNAUTHORIZED, 4038, "헤더에 토큰이 없습니다."),
+    NOT_REGISTERED_USER(HttpStatus.UNAUTHORIZED, 4039, "아직 추가 정보를 입력하지 않은 사용자입니다."),
 
     // Server, File Up/DownLoad Error
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, 5000, "서버 내부에 에러가 발생했습니다."),
